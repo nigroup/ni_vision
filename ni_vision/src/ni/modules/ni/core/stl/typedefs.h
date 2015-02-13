f714f3c136609f49cfd78f00a2a1330eb289ef6f
Youssef Kashef <youssef.kashef@gmail.com>
2015-02-13 16:32:47 +0100

    initial STL typedefs

---
+++ ni_vision/src/ni/modules/ni/core/stl/typedefs.h
@@ -0,0 +1,14 @@
+/** @file convinience typedefs around STL classes
+ */
+#ifndef _NI_CORE_STL_TYPEDEFS_H_
+#define _NI_CORE_STL_TYPEDEFS_H_
+
+#include <vector>
+
+namespace ni {
+
+typedef std::vector<int> VecI;  ///< convinience typedef for vector of integers
+
+} // namespace ni
+
+#endif // _NI_CORE_STL_TYPEDEFS_H_
