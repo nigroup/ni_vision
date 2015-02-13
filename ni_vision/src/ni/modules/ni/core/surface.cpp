85a794c1a4e558bf077938249a91f21cf6771ccf
Youssef Kashef <youssef.kashef@gmail.com>
2015-02-13 16:32:09 +0100

    add Surface class

---
+++ ni_vision/src/ni/modules/ni/core/surface.cpp
@@ -0,0 +1,23 @@
+#include "ni/core/surface.h"
+
+using namespace std;
+using namespace ni;
+
+Surface::Surface()
+{
+}
+
+int Surface::pixelCount()
+{
+    return static_cast<int>(pixel_indices_.size());
+}
+
+void Surface::pixelIndices(const VecI &v)
+{
+    pixel_indices_ = v;
+}
+
+void Surface::pixelIndices() const
+{
+    return pixel_indices_;
+}
---
+++ ni_vision/src/ni/modules/ni/core/surface.h
@@ -0,0 +1,51 @@
+#ifndef _NI_CORE_SURFACE_H_
+#define _NI_CORE_SURFACE_H_
+
+#include "ni/core/stl/typedefs.h"
+
+namespace ni {
+
+/**
+ * @brief class for encapsulating Surface representation and attributes
+ */
+class Surface
+{
+public:
+    /**
+     * @brief count no. of pixels belonging to this surface
+     * @return count of pixels for this surface
+     */
+    int pixelCount();
+
+    /**
+     * @brief Set pixel indicies belonging to this surface
+     *
+     * Involves deep copy
+     *
+     * @param v new vector of indices
+     */
+    void pixelIndices(const VecI &v);
+
+    /**
+     * @brief Get pixel indicies belonging to this surface
+     *
+     * Involves deep copy
+     *
+     * @return pixel indicies
+     */
+    void pixelIndices() const;
+
+    /**
+     * @brief Default Constructor
+     */
+    Surface();
+
+protected:
+
+    // members
+    VecI pixel_indices_;    ///< indicies to pixels belonging to this surface in flattened image
+};
+
+} // namespace ni
+
+#endif // _NI_CORE_SURFACE_H_
