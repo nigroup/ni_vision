58fba8b794db724879ee07399afe57615db816b6
Youssef Kashef <youssef.kashef@gmail.com>
2015-02-13 17:39:48 +0100

    path to OpenCV build not required

--- ni_vision/src/ni/cmake/DetectOpenCV.cmake
+++ ni_vision/src/ni/cmake/DetectOpenCV.cmake
@@ -6,15 +6,17 @@
 # ----------------------------------------------------------------------------
 # Force the user to tell us which OpenCV they want (otherwise find_package can find the wrong one, cache it and changes to OpenCV_DIR are ignored)
 
+set(OPENCV_REQUESTED_COMPONENTS core highgui imgproc)
+
 if(DEFINED OpenCV_DIR)
-    find_package(OpenCV REQUIRED core highgui imgproc PATHS ${OpenCV_DIR})
-    if(OpenCV_FOUND)
-        list(APPEND ${ROOT_PROJECT}_INCLUDE_DIRS ${OpenCV_INCLUDE_DIRS})
-        list(APPEND ${ROOT_PROJECT}_LIBS ${OpenCV_LIBS})
-    else(OpenCV_FOUND)
-        message(SEND_ERROR "Failed to find OpenCV. Double check that \"OpenCV_DIR\" to the root build directory of OpenCV.")
-    endif(OpenCV_FOUND)
+    find_package(OpenCV REQUIRED ${OPENCV_REQUESTED_COMPONENTS} PATHS ${OpenCV_DIR})
 else(DEFINED OpenCV_DIR)
-    set(OpenCV_DIR "" CACHE PATH "Root directory for OpenCV build directory." )
-    message(FATAL_ERROR "\"OpenCV_DIR\" not set. Please explicitly provide the path to the root build directory of OpenCV.")
+    find_package(OpenCV REQUIRED ${OPENCV_REQUESTED_COMPONENTS})
 endif(DEFINED OpenCV_DIR)
+
+if(OpenCV_FOUND)
+    list(APPEND ${ROOT_PROJECT}_INCLUDE_DIRS ${OpenCV_INCLUDE_DIRS})
+    list(APPEND ${ROOT_PROJECT}_LIBS ${OpenCV_LIBS})
+else(OpenCV_FOUND)
+    message(SEND_ERROR "Failed to find OpenCV. Double check that \"OpenCV_DIR\" to the root build directory of OpenCV.")
+endif(OpenCV_FOUND)
