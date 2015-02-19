3985968c378274675deb9014f99c26f905c4961e
Youssef Kashef <youssef.kashef@gmail.com>
2015-02-19 17:12:07 +0100

    enable testing protected methods

--- ni_vision/src/ni/modules/ni/layers/test/depthsegmentation_unittests.cpp
+++ ni_vision/src/ni/modules/ni/layers/test/depthsegmentation_unittests.cpp
@@ -59,9 +59,9 @@ TEST_F(DepthSegmentationTest, Response_dims)
     const int R=10;
     const int C=10;
 
-    for(int r=2; r<R; r++) {
+    for(int r=3; r<R; r++) {
 
-        for(int c=2; c<C; c++) {
+        for(int c=3; c<C; c++) {
 
             Signal sig;
             sig.Append(NAME_GRAD_Y, Mat1f(r, c));
@@ -76,5 +76,78 @@ TEST_F(DepthSegmentationTest, Response_dims)
         }
     }
 }
+
+/**
+ * @brief derive from DepthSegmentation and expose protected methods
+ * to test individually
+ */
+class DepthSegmentationExposeProtected : public DepthSegmentation
+{
+public:
+    DepthSegmentationExposeProtected()
+        : DepthSegmentation()
+    {
+        Clear();
+    }
+
+    DepthSegmentationExposeProtected(const LayerConfig &config)
+        : DepthSegmentation(config)
+    {
+        Clear();
+        Reconfigure(config);
+        IONames(config);
+    }
+
+    bool comparePixels(float current, float neighbor)
+    {
+        return DepthSegmentation::comparePixels(current, neighbor);
+    }
+
+    void group(const cv::Mat1f g)
+    {
+        DepthSegmentation::group(g);
+    }
+};
+
+class DepthSegmentationProtectedTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        config_ = LayerConfig();
+
+        // params
+        max_grad_ = 0.005f;
+        PTree p;
+        p.add(DepthSegmentation::PARAM_MAX_GRAD, max_grad_); // value from paper and first version
+        config_.Params(p);
+
+        // IO
+        config_.Input(DepthSegmentation::KEY_INPUT_STIMULUS, NAME_GRAD_Y);
+        config_.Output(DepthSegmentation::KEY_OUTPUT_RESPONSE, NAME_OUT_SEG_MAP);
+
+        to_.reset(new DepthSegmentationExposeProtected(config_));
+    }
+
+    shared_ptr<DepthSegmentationExposeProtected> to_; ///< test object
+    LayerConfig config_;        ///< default config for tests
+    float max_grad_;
+};
+
+TEST_F(DepthSegmentationProtectedTest, ComparePixels)
+{
+    EXPECT_TRUE(to_->comparePixels(-10.f, -10.f));
+    EXPECT_TRUE(to_->comparePixels(0.f, 0.f));
+    EXPECT_TRUE(to_->comparePixels(10000.f, 10000.f));
+    EXPECT_TRUE(to_->comparePixels(12345.f, -12345.f));
+    EXPECT_TRUE(to_->comparePixels(-12345.f, 12345.f));
+
+    float x = 2.f;
+    EXPECT_FALSE(to_->comparePixels(x, x+max_grad_));
+    EXPECT_TRUE(to_->comparePixels(x, x+max_grad_-1e-5));
+    EXPECT_FALSE(to_->comparePixels(x+max_grad_, x));
+    EXPECT_TRUE(to_->comparePixels(x+max_grad_-1e-5, x));
+}
+
 
 } // annonymous namespace for test cases and fixtures
