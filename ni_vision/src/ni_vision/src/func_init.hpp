623cc829fb9daf13d0a62b0423432c6f0946900c
Youssef Kashef <youssef.kashef@gmail.com>
2015-03-13 14:48:42 +0100

    separate SurfProp members declarations over multiple lines

--- ni_vision/src/ni_vision/src/func_init.hpp
+++ ni_vision/src/ni_vision/src/func_init.hpp
@@ -79,9 +79,12 @@ std::string sDataDir;
 
 
 struct SurfProp {
-    std::vector<int> vnIdx, vnPtsCnt; std::vector<std::vector<int> > mnPtsIdx;
-    std::vector<std::vector<int> > mnRect, mnRCenter; std::vector<std::vector<float> > mnCubic, mnCCenter;
-    std::vector<float> vnLength; std::vector<std::vector<float> > mnColorHist;
+    std::vector<int> vnIdx, vnPtsCnt;
+    std::vector<std::vector<int> > mnPtsIdx;
+    std::vector<std::vector<int> > mnRect, mnRCenter;
+    std::vector<std::vector<float> > mnCubic, mnCCenter;
+    std::vector<float> vnLength;
+    std::vector<std::vector<float> > mnColorHist;
     std::vector<int> vnMemCtr, vnStableCtr, vnLostCtr;
     std::vector<int> vnFound;
 };
