fff8dfc28e3b65dd1d11a8c12d129d464566b558
Youssef Kashef <youssef.kashef@gmail.com>
2015-03-16 12:00:22 +0100

    bring over color histogram routine, originally Calc3DColorHistogram() in func_operations.hpp

---
+++ ni_vision/src/ni/modules/ni/core/colorhistogram.cpp
@@ -0,0 +1,49 @@
+#include "ni/core/colorhistogram.h"
+
+#include <opencv2/core/core.hpp>
+
+using namespace std;
+using namespace cv;
+using namespace elm;
+using namespace ni;
+
+void ni::computeColorHist(const Mat1f &src, const VecF &indices, int nb_bins, Mat1f &dst)
+{
+    dst = Mat1f::zeros(1, 3*nb_bins);
+
+    int nb_bins_sq = nb_bins*nb_bins;
+
+    // numerator has to be slightly greater than 1, otherwise there is a problem if a channel is equal to 255
+    float bin_width = 1.0001f/static_cast<float>(nb_bins);
+
+    float* data_ptr = reinterpret_cast<float*>(src.data);
+
+    for(size_t i=0; i<indices.size(); i++) {
+
+        int pixel = i*3;
+        float _b = data_ptr[pixel];
+        float _g = data_ptr[pixel+1];
+        float _r = data_ptr[pixel+2];
+
+        float sum = _b + _g + _r;
+        if(sum != 0) {
+
+            _b /= sum;
+            _g /= sum;
+            _r /= sum;
+        }
+        else {
+            _b = _g = _r = 1.f/3.f; // why so for black pixels?
+        }
+
+        // perform binning
+        int bin_b = static_cast<int>(_b/bin_width);
+        int bin_g = static_cast<int>(_g/bin_width);
+        int bin_r = static_cast<int>(_r/bin_width);
+
+        // changing color order from BGR to RGB
+        dst(bin_r*nb_bins_sq + bin_g*nb_bins + bin_b)++;
+    }
+
+    dst /= static_cast<float>(indices.size()); // normalizing
+}
---
+++ ni_vision/src/ni/modules/ni/core/colorhistogram.h
@@ -0,0 +1,29 @@
+#ifndef _NI_CORE_COLORHISTOGRAM_H_
+#define _NI_CORE_COLORHISTOGRAM_H_
+
+#include "elm/core/cv/typedefs_fwd.h"
+#include "elm/core/stl/typedefs.h"
+
+namespace ni {
+
+/**
+ * @brief compute color histogram over subset of image pixels
+ *
+ * This method is used to generate the object libraries both by makelib_simple and ni_vision
+ * It classifies every pixel into one bin in every channel. With 3 channels and 8 bins per channel that gives
+ * 512 possible values for each pixel. The result is saved in a vector of that length, which saves the count
+ * of pixel which have a certain value. Later on this vector is then normalized
+ * such that the sum over all entries is 1.
+ *
+ * @param src continuous bgr color image
+ * @param indices subset of pixels to process
+ * @param nb_bins no. of histogram bins per color channel
+ * @param[out] dst color historam (in rgb order)
+ *
+ * @todo use OpenCV's calcHist(), keep everything in bgr color order
+ */
+void computeColorHist(const cv::Mat1f &src, const elm::VecF &indices, int nb_bins, cv::Mat1f &dst);
+
+} // namespace ni
+
+#endif // _NI_CORE_COLORHISTOGRAM_H_
