2ed6278f60efe6242c362ba71640a53195f40a2d
Youssef Kashef <youssef.kashef@gmail.com>
2015-03-20 18:27:38 +0100

    start moving legacy code into a legacy module, update legacy ni_vision node to depend on ni package, particulalry its legacy module

--- ni_vision/src/ni_vision/src/func_init.hpp
+++ ni_vision/src/ni_vision/src/func_init.hpp
@@ -11,7 +11,7 @@
  * represent the button.
  */
 
-#include "surfprop.h"
+#include "ni/legacy/surfprop.h"
 
 int64_t timespecDiff (struct timespec *timeA_p, struct timespec *timeB_p) {
   return ((timeA_p->tv_sec * 1000000000) + timeA_p->tv_nsec) - ((timeB_p->tv_sec * 1000000000) + timeB_p->tv_nsec);
